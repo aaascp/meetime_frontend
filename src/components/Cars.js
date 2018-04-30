472d51b51f7724da4190bbc9118683e02a99ff0d
Andre Alex Araujo <andrealexaraujo@gmail.com>
2018-04-30 15:37:34 -0300

    fixing form style

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -6,13 +6,15 @@ import CarsForm from "./forms/cars/CarsForm";
 class Cars extends React.Component {
   render() {
     return (
-      <div className="container split-pane">
-        <div className="container split-pane__left">
-          <CarsList />
-        </div>
+      <div className="container">
+        <div className="split-pane">
+          <div className="split-pane__item">
+            <CarsList />
+          </div>
 
-        <div className="container split-pane__right">
-          <CarsForm />
+          <div className="split-pane__item split-pane__item--right">
+            <CarsForm />
+          </div>
         </div>
       </div>
     );
