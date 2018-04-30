65f3a1534b54b76a51093391e22f50bd96f35f6c
Andre Alex Araujo <andrealexaraujo@gmail.com>
2018-04-30 19:28:32 -0300

    adjustin split pane

--- src/components/forms/cars/CarsForm.js
+++ src/components/forms/cars/CarsForm.js
@@ -68,7 +68,7 @@ class CarsForm extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="form__title big-only">
+        <h1 className="form__title">
           {this.props.isUpdate ? "Atualizar" : "Novo"} Carro
         </h1>
         <form
